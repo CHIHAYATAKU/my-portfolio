8fb72808c64eff9ec058b1b2e5e6156870dec9e3
chihaya <takuchi.1071880@gmail.com>
2024-05-02 22:06:27 +0900

    header created

---
+++ src/app/components/layouts/ETRobocon/ETRobocon.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ETRobocon = () => {
+    return (
+        <div>ETRobocon</div>
+    )
+}
--- src/app/components/layouts/header/Header.tsx
+++ src/app/components/layouts/header/Header.tsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from "next/image";
+import Script from 'next/script';
+import mylogo from './Header_images/logo_takuma.png';
+import styles from "./Header.module.css";
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <header id={styles.top}>
+            <div className={styles.container}>
+                <div>
+                    <a href={"/"}>
+                        <Image id={styles.header_logo} src={mylogo} alt="Takuma Portforio" />
+                    </a>
+                </div>
+                <nav id={styles.navlist}>
+                    <ul>
+                        <li className={styles.topnav}>
+                            <a className={styles.a} href="#">ABOUT</a>
+                        </li>
+                        <li className={styles.topnav}>
+                            <a className={styles.a} href="#">ETROBOCON</a>
+                        </li>
+                        <li className={styles.topnav}>
+                            <a className={styles.a} href="#">PRODUCTS</a>
+                        </li>
+                        <li className={styles.topnav}>
+                            <a className={styles.a} href="#">UNIV</a>
+                        </li>
+                        <li className={styles.topnav}>
+                            <a className={styles.a} href="#">PRIVATE</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header >
     )
 }
 
