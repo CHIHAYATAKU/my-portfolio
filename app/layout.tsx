f67a9bc46998d44c8ad04be4f570e1026a3bf3f0
chihaya <takuchi.1071880@gmail.com>
2024-06-12 15:39:01 +0900

    loading試し

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Loading from "./Loading";
 import { BgAnimation } from "./_components/layouts/BgAnimation";
 import { BackToTopButton } from "./_components/layouts/BackToTopButton";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,16 +26,19 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
+
       <body className={inter.className}>
-        <Loading />
-        <div className="wrapper">
-          <BgAnimation />
-          <Header />
-          <div id="pagetop"></div>
-          {children}
-        </div>
-        <BackToTopButton />
-        <Footer />
+        <Suspense fallback={<Loading />}>
+          {/* <Loading /> */}
+          <div className="wrapper">
+            <BgAnimation />
+            <Header />
+            <div id="pagetop"></div>
+            {children}
+          </div>
+          <BackToTopButton />
+          <Footer />
+        </Suspense>
       </body>
     </html >
   );
