beb3dc11f6f3355b20696a24d4dccd5967feb084
chihaya <takuchi.1071880@gmail.com>
2024-06-18 15:36:07 +0900

    refactor: 同じ処理をmap関数で展開

--- app/_features/TopPage/components/TopPrivate/index.tsx
+++ app/_features/TopPage/components/TopPrivate/index.tsx
@@ -3,10 +3,16 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { LinkButton } from 'app/_components/elements/LinkButton'
 import styles from "./TopPrivate.module.css"
-import private1 from "./TopPrivate_Images/private1.jpg"
+// import private1 from "./TopPrivate_Images/private1.jpg"
 import private2 from "./TopPrivate_Images/private2.jpg"
 import private3 from "./TopPrivate_Images/private3.jpg"
 import sampleImg from "public/Images/sample.jpg"
+
+const PRIVATE_ITEM = [
+    { src: sampleImg, label: "玉置浩二コンサート" },
+    { src: private2, label: "愛ネコ" },
+    { src: private3, label: "私と景色" },
+];
 
 export const TopPrivate = () => {
     return (
@@ -15,24 +21,18 @@ export const TopPrivate = () => {
                 <div className={styles.container}>
                     <h2 className={styles.h2}>PRIVATE</h2>
                     <div className={styles.imgassets}>
-                        <div className={styles.element}>
-                            <Image className={styles.private} src={sampleImg} alt='private1' />
-                            <p className={styles.p}>
-                                玉置浩二コンサート
-                            </p>
-                        </div>
-                        <div className={styles.element}>
-                            <Image className={styles.private} src={private2} alt='private2' />
-                            <p className={styles.p}>
-                                愛ネコ
-                            </p>
-                        </div>
-                        <div className={styles.element}>
-                            <Image className={styles.private} src={private3} alt='private3' />
-                            <p className={styles.p}>
-                                私と景色
-                            </p>
-                        </div>
+                        {PRIVATE_ITEM.map((item, index) => {
+                            return (
+                                <>
+                                    <div className={styles.element}>
+                                        <Image className={styles.private} src={item.src} alt={item.label} />
+                                        <p className={styles.p}>
+                                            {item.label}
+                                        </p>
+                                    </div>
+                                </>
+                            )
+                        })}
                     </div>
                     <Link href={"Private"} className="button"><LinkButton /></Link>
                 </div>
--- app/_features/TopPage/components/TopProducts/index.tsx
+++ app/_features/TopPage/components/TopProducts/index.tsx
@@ -6,6 +6,13 @@ import styles from "./TopProducts.module.css"
 import Product1 from "./TopProducts_Images/product1.png"
 import Product2 from "./TopProducts_Images/product2.png"
 
+const PRODUCTS_ITEM = [
+    { src: Product1, label: "ポートフォリオサイト" },
+    { src: Product2, label: "ギター練習アプリ" },
+    { src: Product1, label: "更新をお待ちください" },
+];
+
+
 export const TopProducts = () => {
     return (
         <>
@@ -13,24 +20,18 @@ export const TopProducts = () => {
                 <div className={styles.container}>
                     <h2 className={styles.h2}>PRODUCTS</h2>
                     <div className={styles.imgassets}>
-                        <div className={styles.element}>
-                            <Image className={styles.products} src={Product1} alt='product1' />
-                            <p className={styles.p}>
-                                ポートフォリオサイト
-                            </p>
-                        </div>
-                        <div className={styles.element}>
-                            <Image className={styles.products} src={Product2} alt='product2' />
-                            <p className={styles.p}>
-                                ギター練習アプリ
-                            </p>
-                        </div>
-                        <div className={styles.element}>
-                            <Image className={styles.products} src={Product1} alt='product3' />
-                            <p className={styles.p}>
-                                更新までお待ちください
-                            </p>
-                        </div>
+                        {PRODUCTS_ITEM.map((item, index) => {
+                            return (
+                                <>
+                                    <div className={styles.element}>
+                                        <Image className={styles.products} src={item.src} alt={item.label} />
+                                        <p className={styles.p}>
+                                            {item.label}
+                                        </p>
+                                    </div>
+                                </>
+                            )
+                        })}
                     </div>
                     <Link href={"Products"} className="button"><LinkButton /></Link>
                 </div>
