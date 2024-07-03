0cb63675deaae0fb3b6e42d60c9cb6ea813ddba0
chihaya <takuchi.1071880@gmail.com>
2024-07-03 12:31:05 +0900

    ファイル名の変更

--- app/_features/Univ/components/UnivClub/index.tsx
+++ app/_features/Univ/components/UnivClub/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from "./UnivClub.module.css"
-import { Video } from './ClubVideoes/video'
+import { Video } from './ClubVideoes/index'
 
 export const UnivClub = () => {
     return (
