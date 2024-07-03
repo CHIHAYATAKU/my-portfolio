c98f22f90e1f6519c119728efd1df0e81b09ce09
chihaya <takuchi.1071880@gmail.com>
2024-07-03 12:23:52 +0900

    public/は除いた指定で行う

--- app/_features/Univ/components/UnivClub/ClubVideoes/video.tsx
+++ app/_features/Univ/components/UnivClub/ClubVideoes/video.tsx
@@ -3,12 +3,12 @@ import styles from "./video.module.css"
 export function Video() {
     return (
         <>
-            <video className={styles.video} controls preload="none" playsInline src="_next/static/media/lupin.mp4" />
-            {/* <source src="/public/UnivClub_Videoes/lupin.mp4" type="video/mp4" />
-                Your browser does not support the video tag. */}
-            {/* </video> */}
             <video className={styles.video} controls preload="none" playsInline>
-                <source src="./UnivClub_Videoes/tabiji.mp4" type="video/mp4" />
+                <source src="/UnivClub_Videoes/lupin.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+            </video >
+            <video className={styles.video} controls preload="none" playsInline>
+                <source src="/UnivClub_Videoes/tabiji.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
         </>
