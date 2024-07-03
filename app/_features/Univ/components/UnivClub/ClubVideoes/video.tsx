73ca95c533be5355bd0f56d87c36c8471b1931e4
chihaya <takuchi.1071880@gmail.com>
2024-07-03 11:56:39 +0900

    ためし

--- app/_features/Univ/components/UnivClub/ClubVideoes/video.tsx
+++ app/_features/Univ/components/UnivClub/ClubVideoes/video.tsx
@@ -3,10 +3,10 @@ import styles from "./video.module.css"
 export function Video() {
     return (
         <>
-            <video className={styles.video} controls preload="none" playsInline>
-                <source src="./UnivClub_Videoes/lupin.mp4" type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
+            <video className={styles.video} controls preload="none" playsInline src="./UnivClub_Videoes/lupin.mp4" />
+            {/* <source src="/public/UnivClub_Videoes/lupin.mp4" type="video/mp4" />
+                Your browser does not support the video tag. */}
+            {/* </video> */}
             <video className={styles.video} controls preload="none" playsInline>
                 <source src="./UnivClub_Videoes/tabiji.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
